db13d758a94bad8958d61cfdd2b01b308600b344
coding-wizard6 <sathvik.yk2@gmail.com>
2024-11-16 21:27:23 +0530

    index.html

---
+++ Module 4/Lesson 1/script.js
@@ -0,0 +1,5 @@
+document.getElementById("heading1").innerHTML = "Basic Javasript Code"
+function display() {
+    //    document.write("hello world")
+    document.getElementById("para").innerHTML = "Sathvik"
+}
