2053e8096a7b4934ead56317ae0156d55caecad8
coding-wizard6 <sathvik.yk2@gmail.com>
2024-12-28 21:48:01 +0530

    datatypes and operators

---
+++ Module 12/Lesson 2/datatype.py
@@ -0,0 +1,11 @@
+name="sathvik"
+age=14
+percentage=96.9
+
+#print(name,type(name),age,type(age),percentage,type(percentage))
+
+age1=float(age)
+print(age1)
+
+percentage1=int(percentage)
+print(percentage1)
---
+++ Module 12/Lesson 2/operator.py
@@ -0,0 +1,15 @@
+n1=2
+n2=3
+
+print("addition",n1 + n2)
+print("subtraction",n1 - n2)
+print("multiplication",n1 * n2)
+print("division",n1 / n2)
+print("floor",n1//n2)
+print("remainder",n1%n2)
+print("exponent",n1**n2)
+
+print(n1==n2)
+print(n1!=n2)
+print(n1>=n2)
+print(n1<=n2)
