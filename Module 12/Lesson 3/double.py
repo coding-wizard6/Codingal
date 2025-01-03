04b72939570962eb36114b13bdd11de853b3e15a
coding-wizard6 <sathvik.yk2@gmail.com>
2025-01-03 21:41:23 +0530

    double.py

---
+++ Module 12/Lesson 3/double.py
@@ -0,0 +1,12 @@
+num= int(input("Enter number"))
+
+if num>=50:
+    print("The number is greater than 50")
+
+    if num%2==0:
+        print("its an even number")
+    else:
+        print("its an odd number")
+
+else:
+    print("The number is less than 50")
