9f556050778946154f57a60be8e50c8f2b0a9982
coding-wizard6 <sathvik.yk2@gmail.com>
2025-01-10 20:16:45 +0530

    func.py

---
+++ Module 12/Lesson 5/func.py
@@ -0,0 +1,8 @@
+def myself(name,age):
+    print("My name is ",name,". I'm ",age," years old.")
+    return name,age
+
+name1=input("What is your name?: ")
+age1= int(input("What is your age?: "))
+
+myself(name1,age1)
