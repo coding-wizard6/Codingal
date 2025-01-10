f03707ab0c47819687e025396585cb145153a41d
coding-wizard6 <sathvik.yk2@gmail.com>
2025-01-10 20:46:22 +0530

    calc.py

---
+++ Module 12/Lesson 5/calc.py
@@ -0,0 +1,19 @@
+def add(n,m):
+    return n+m
+def subtract(n,m):
+    return n-m
+def multiply(n,m):
+    return n*m
+def divide(n,m):
+    return n/m
+
+num1=int(input("Enter num1: "))
+num2=int(input("Enter num2: "))
+
+print("Addition- ",add(num1,num2))
+
+print("Subtration- ",subtract(num1,num2))
+
+print("Multiplication- ",multiply(num1,num2))
+
+print("Division- ",divide(num1,num2))
